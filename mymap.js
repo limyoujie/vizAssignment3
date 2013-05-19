3408024f13cf889e8e830c4ec55849b3d5989331
fooshnip <riley.jos@gmail.com>
2013-05-19 15:24:34 -0500

    working symbol map

--- mymap.js
+++ mymap.js
@@ -5,7 +5,8 @@ var radius = d3.scale.sqrt()
     .domain([0, 1e6])
     .range([0, 10]);
 
-var albers = d3.geo.albersUsa();
+var albers = d3.geo.albersUsa()
+.scale(875);
 
 var path = d3.geo.path().projection(albers);
 
@@ -29,8 +30,18 @@ function ready(error, us, circles) {
   svg.selectAll("circle")
       .data(circles)
     .enter().append("circle")
+      .attr("cx", function(d) { return albers([d.y,d.x])[0]*Math.random()*3})
+      .attr("cy", function(d) { return albers([d.y,d.x])[1]*Math.random()-100})
+      .attr("r", function(d) { return d.Attribute1/2})
+      .style("fill", "transparent")
+      .style("stroke", "red")
+      .style("stroke-width",4)
+      .transition()
+      .duration(300)
       .attr("cx", function(d) { return albers([d.y,d.x])[0]})
       .attr("cy", function(d) { return albers([d.y,d.x])[1]})
-      .attr("r", function(d) { return d.Attribute1/4});
+      .attr("r", function(d){ return d.Attribute1/4})
+      .style("stroke","black")
+      .style("stroke-width",1);
 
 }
