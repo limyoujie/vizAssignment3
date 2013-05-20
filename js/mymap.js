131725620e1b4760497e2ded924c40d2db25b522
fooshnip <riley.jos@gmail.com>
2013-05-19 20:07:23 -0500

    map and navbar update

--- js/mymap.js
+++ js/mymap.js
@@ -1,17 +1,20 @@
-var width = 960,
+var margin = {top: 20, right: 10, bottom: 20, left: 40},
+    width = 960,
     height = 500,
-    active;
+    active;    
 
 var projection = d3.geo.albersUsa()
     .scale(1000)
     .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
-    .projection(projection);
+    .projection(projection);    
 
 var svg = d3.select("#mymap").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", width + margin.left)
+    .attr("height", height + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 svg.append("rect")
     .attr("width", width)
@@ -51,7 +54,7 @@ function ready(error, us, circles) {
       .style("stroke", "red")
       .style("stroke-width",4)
       .transition()
-      .duration(300)
+      .duration(10000)
       .attr("cx", function(d) { return projection([d.y,d.x])[0]})
       .attr("cy", function(d) { return projection([d.y,d.x])[1]})
       .attr("r", function(d){ return d.Attribute1/4})
@@ -68,7 +71,7 @@ function click(d) {
   var b = path.bounds(d);
   g.transition().duration(750).attr("transform",
       "translate(" + projection.translate() + ")"
-      + "scale(" + .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height) + ")"
+      + "scale(" + .85 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height) + ")"
       + "translate(" + -(b[1][0] + b[0][0]) / 2 + "," + -(b[1][1] + b[0][1]) / 2 + ")");
 }
 
