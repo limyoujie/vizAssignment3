ed10d5f531cbc7374f762b3c4ba8f80ac515c11c
fooshnip <riley.jos@gmail.com>
2013-05-22 21:13:04 -0500

    mavi code merge

--- js/PseudoDemersCartogram.js
+++ js/PseudoDemersCartogram.js
@@ -2,6 +2,9 @@ var color = d3.scale.linear()
               .domain([0, 10])
               .range(["blue", "purple"]);
 
+var trans=[75,120],
+    scale=1;
+
 var margin = {top: -20, right: 20, bottom: 20, left: 20},
     padding = {top: 60, right: 60, bottom: 60, left: 60},
     outerWidth = 450,
@@ -56,19 +59,36 @@ queue()
     .await(ready);    
 
 function ready(error, us, fakedata, states) {
+  
+  //LET'S GET IT ON
+  d3.select("#fight")
+    .on("click", fight);
+
+  var ProvidersPerState = {}
   var nodes = states.features
       .map(function(d) {
         var point = projection(d.geometry.coordinates),
-            abb = d.properties.abb;
-            namestate = d.properties.name;
-            ATT = d.properties.ATT;
+            abb = d.id,
+            namestate = d.properties.abb,
+            namecomp = d.properties.name,
+            ATT = d.properties.ATT,
+            Cellco = d.properties.Cellco,
+            Sprint = d.properties.Sprint,
+            TMobile = d.properties.TMobile;
+            ProvidersPerState[namestate] = [ATT, Cellco, Sprint, TMobile];
 
         return {
-          x: point[0]-120, y: point[1]+150,
-          x0: point[0]-120, y0: point[1]+150,
-          r: radius(ATT), name: namestate,
-          abb: abb,
-          att: ATT
+          x: point[0]-120, y: 
+          point[1]+90,
+          x0: point[0]-120, 
+          y0: point[1]+90,
+          r: radius(5),
+          namecomp: namecomp, 
+          name: namestate,
+          att: ATT,
+          cellco: Cellco,
+          sprint: Sprint,
+          tmobile: TMobile
         };
       });
   force
@@ -90,19 +110,18 @@ function ready(error, us, fakedata, states) {
 
     node.append("rect")
       .attr("class", function(d) { return d.name + ' squares';})
+      .attr("id", function(d) {return d.name;})
       .attr("width", function(d) { return d.r * 2; })
       .attr("height", function(d) { return d.r * 2; })
       .style("stroke", "white")
-      .style("fill", function(d) { return color(d.att); });
-
-    console.log(d3.select("rect")
-              .data(nodes)
-              .attr("name","Alabama"));
+      .style("fill", function(d) { return color(d.att); })
+      .on("mouseover",minimouseover)
+      .on("mouseout",minimouseout);
 
     node.append("text")
       .attr("dx", function(d) { return d.r/2;})
       .attr("dy", function(d) { return d.r;})
-      .text(function(d) { return d.abb; })
+      .text(function(d) { return d.name; })
       .style("font-family", "Arial")
       .style("font-size", function(d) {return (d.value + 100) + " px";})
       .style("fill", "white")
@@ -127,7 +146,7 @@ function ready(error, us, fakedata, states) {
           .data(nodes)
           .style("fill", function(d) { return color(d.att); })
           .on("mouseover",minimouseover)
-          .on("mouseout",minimouseout);;
+          .on("mouseout",minimouseout);
 
 function minimouseover(d){
 
@@ -136,92 +155,40 @@ function minimouseover(d){
       d3.select(this)
         .style("stroke","black");
 
-      d3.selectAll("."+name)
-        .style("fill", "white")
-        .style("stroke-width", 3);
-    }
+      d3.selectAll("."+d.name)
+        .style("fill", function(d) { return "gold"; })
+        .style("stroke-width", 1);
 
-function minimouseout(){
+      $("#pop-up").fadeOut(100,function () {
+          // Popup content
+          $("#pop-up-title").html(d.namecomp);
+          $("#pop-img").html(name);
+          $("#pop-desc").html("In "+name+", the winner is: "+d.ATT);
+
+          // Popup position
+          var popLeft = (d.x*scale)+trans[0]+20;//lE.cL[0] + 20;
+          var popTop = (d.y*scale)+trans[1]+20;//lE.cL[1] + 70;
+          $("#pop-up").css({"left":popLeft,"top":popTop});
+          $("#pop-up").fadeIn(100);
+      });
+  }
+
+function minimouseout(d){
+
+      var name = d.name;
       d3.select(this)
         .style("stroke","white");
 
-      node.selectAll("rect")
+      d3.selectAll("."+name)
+        .attr("width", function(d) { return d.r * 2; })
+        .attr("height", function(d) { return d.r * 2; })
         .style("fill", function(d) { return color(d.att); })
         .style("stroke-width", 1);
+
+      $("#pop-up").fadeOut(50);
+        d3.select("."+name).attr("fill","url(#ten1)");
     }
 //////////////MINIMAP END////////////////////
-
-/////////TITLE DATA//////////////////////
-
-var yadjust=60
-
-    svg1.append("text")
-      .text("AT&T")
-      .style("font-size", "66px")
-      .style("font-family", "Yanone Kaffeesatz")
-      .style("fill","purple")
-      .attr("dx", -100)
-      .attr("dy", 70+yadjust)
-    .transition()
-      .delay(500)
-      .ease("bounce")
-      .duration(2500)
-      .attr("dx", 300)
-      .attr("dy", 70+yadjust)
-    .transition()
-      .ease("linear")
-      .attr("dx", 200)
-      .attr("dy", 70+yadjust);
-
-    svg1.append("text")
-      .text("vs.")
-      .style("font-size", "136px")
-      .style("font-family", "Yanone Kaffeesatz")
-      .style("fill","grey")
-      .attr("dx", 350)
-      .attr("dy", -100+yadjust)
-    .transition()
-      .ease("backs")
-      .delay(1250)
-      .duration(4000)
-      .attr("dx", 350)
-      .attr("dy", 80+yadjust);
-
-    svg1.append("text")
-      .text("Verizon")
-      .style("font-size", "66px")
-      .style("font-family", "Yanone Kaffeesatz")
-      .style("fill","green")
-      .attr("dx", 1000)
-      .attr("dy", 80+yadjust)
-    .transition()
-      .delay(500)
-      .ease("bounce")
-      .duration(2500)
-      .attr("dx", 400)
-      .attr("dy", 80+yadjust)
-    .transition()
-      .ease("linear")
-      .attr("dx", 500)
-      .attr("dy", 80+yadjust);
-
-  setInterval(4000,fireworks());
-
-  function fireworks() {
-    var transforms = ["100,190","100,220","100,20","-100,200","-100,30","-100,220","-100,20","-200,90",
-    "-200,180","-200,80","200,90","200,190","200,170","200,70","-50,180","-50,80",
-    "50,180","50,150"];
-    for (var i = 0; i < transforms.length; i++) {
-      svg1.append("svg:circle")
-        .attr("cx",400).attr("cy",45).attr("r",0)
-        .style("stroke","yellow").style("fill","grey").style("stroke-opacity",0.5)
-        .transition()
-          .attr("transform","translate("+transforms[i]+")").delay(5000).duration(2000).ease(Math.sqrt).attr("r",Math.random()*30)
-          .style("stroke-opacity",1e-6).style("fill-opacity",1e-6).remove();
-    }
-  }
-
-  /////////////////TITLE DATA/////////////////
 
   function tick(e) {
     node.each(gravity(e.alpha * .1))
@@ -267,5 +234,163 @@ var yadjust=60
       });
     };
   }
-};
 
+  function fight(){  
+
+        var Player1 = document.getElementById('firstbox').value;
+        var Player2 = document.getElementById('secondbox').value;
+        var Player1k = document.getElementById('myselection1').innerHTML;
+        var Player2k = document.getElementById('myselection2').innerHTML;
+        var PointsPlayer1 =0; 
+        var PointsPlayer2 = 0;
+        var Tota = [];
+
+        if(Player1!=Player2 & Player1!=-1 & Player2!=-1){
+            for(key in ProvidersPerState){
+                
+                // Chanhing Size and Colors
+                node.selectAll("#"+key)
+                .transition()
+                .delay(5000)
+                .ease("linear")
+                .attr("width", function(d) { if (ProvidersPerState[key][Player1]==0 & ProvidersPerState[key][Player2]==0){
+                                              return 80;
+                                              }
+                                              else {
+                                              return Math.abs(ProvidersPerState[key][Player1]-ProvidersPerState[key][Player2])*10;  
+                                              }})
+                .attr("height", function(d) { if (ProvidersPerState[key][Player1]==0 & ProvidersPerState[key][Player2]==0){
+                                              return 80;
+                                              }
+                                              else {
+                                              return Math.abs(ProvidersPerState[key][Player1]-ProvidersPerState[key][Player2])*10;  
+                                              }}) 
+                .style("fill", function(d) {  if (ProvidersPerState[key][Player1]==0 & ProvidersPerState[key][Player2]==0){
+                                              return "purple"
+                                              }
+                                              else if (ProvidersPerState[key][Player1]>ProvidersPerState[key][Player2]){
+                                                  return "orange";
+                                              } 
+                                              else if (ProvidersPerState[key][Player1]<ProvidersPerState[key][Player2]){
+                                                  return "blue";
+                                              }
+                                              else {return "green";}
+                                              })
+                .on("mouseover",minimouseover)
+                .on("mouseout",minimouseout);
+
+                // Text
+                // d3.select("#"+key+"Text")
+                // .attr("dx", function(d) { return Math.abs(ProvidersPerState[key][Player1]-ProvidersPerState[key][Player2]);})
+                // .attr("dy", function(d) { return Math.abs(ProvidersPerState[key][Player1]-ProvidersPerState[key][Player2]);})
+                // .text(function(d) { return d.key; })
+                // .style("font-size", function(d) {return Math.abs(ProvidersPerState[key][Player1]-ProvidersPerState[key][Player2])})
+                // .style("fill", "white")
+                // .style("cursor", "default");
+
+                    // Defining the winner
+                    if (ProvidersPerState[key][Player1]>ProvidersPerState[key][Player2]){
+                        PointsPlayer1 = PointsPlayer1+1;
+                    }
+                    else if (ProvidersPerState[key][Player1]<ProvidersPerState[key][Player2]){
+                        PointsPlayer2 = PointsPlayer2+1;
+                    }
+
+            }
+
+            Total = [50, 25];
+            SumTotal = PointsPlayer1+ PointsPlayer2;
+
+            node.append("rect")
+              .data(Total)
+              .attr("class", "TotalPoints")
+              .attr("width", function(d) { return d; })
+              .attr("height", function(d) { return d; })
+              .style("stroke", "white")
+              .style("fill", "orange");
+
+            /////////TITLE DATA//////////////////////
+
+            var yadjust=60
+            
+            svg1.selectAll(".titletext")
+              .transition()
+                .remove();
+
+            svg1.append("text")
+              .attr("class","titletext")
+              .text(Player1k)
+              .style("font-size", "66px")
+              .style("font-family", "Yanone Kaffeesatz")
+              .style("fill","purple")
+              .attr("dx", -100)
+              .attr("dy", 70+yadjust)
+            .transition()
+              .delay(500)
+              .ease("bounce")
+              .duration(2500)
+              .attr("dx", 300)
+              .attr("dy", 70+yadjust)
+            .transition()
+              .ease("linear")
+              .attr("dx", 200)
+              .attr("dy", 70+yadjust);
+
+            svg1.append("text")
+              .attr("class","titletext")
+              .text("vs.")
+              .style("font-size", "136px")
+              .style("font-family", "Yanone Kaffeesatz")
+              .style("fill","grey")
+              .attr("dx", 350)
+              .attr("dy", -100+yadjust)
+            .transition()
+              .ease("backs")
+              .delay(1250)
+              .duration(4000)
+              .attr("dx", 350)
+              .attr("dy", 80+yadjust);
+
+            svg1.append("text")
+              .text(Player2k)
+              .attr("class","titletext")
+              .style("font-size", "66px")
+              .style("font-family", "Yanone Kaffeesatz")
+              .style("fill","green")
+              .attr("dx", 1000)
+              .attr("dy", 80+yadjust)
+            .transition()
+              .delay(500)
+              .ease("bounce")
+              .duration(2500)
+              .attr("dx", 400)
+              .attr("dy", 80+yadjust)
+            .transition()
+              .ease("linear")
+              .attr("dx", 500)
+              .attr("dy", 80+yadjust);
+
+          setInterval(4000,fireworks());
+
+          function fireworks() {
+            var transforms = ["100,190","100,220","100,20","-100,200","-100,30","-100,220","-100,20","-200,90",
+            "-200,180","-200,80","200,90","200,190","200,170","200,70","-50,180","-50,80",
+            "50,180","50,150"];
+            for (var i = 0; i < transforms.length; i++) {
+              svg1.append("svg:circle")
+                .attr("cx",400).attr("cy",45).attr("r",0)
+                .style("stroke","yellow").style("fill","grey").style("stroke-opacity",0.5)
+                .transition()
+                  .attr("transform","translate("+transforms[i]+")").delay(5000).duration(2000).ease(Math.sqrt).attr("r",Math.random()*30)
+                  .style("stroke-opacity",1e-6).style("fill-opacity",1e-6).remove();
+            }
+          }
+
+          /////////////////TITLE DATA END/////////////////
+        }
+        else {
+                alert("Please Select Player 1 and Player 2")
+        }
+  }
+
+};
